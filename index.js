7ce2e431e892313042a3df27bcee016f4d6e06c6
dahiyasunil <sunil.dahiya13@gmail.com>
2025-01-02 11:30:44 +0530

    feat: added functionaltiy for get and post movies

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ app.use(cors(corsOption));
 app.use(express.json());
 
 initialiseDatabase();
+
+app.get("/movies", async (req, res) => {
+  try {
+    const allMovies = await MoviesDB.find();
+    res.status(200).json(allMovies);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/movies", async (req, res) => {
+  try {
+    const newMovie = new MoviesDB(req.body);
+    await newMovie.save();
+    res.status(201).json(bookData);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is up and running!");
+});
