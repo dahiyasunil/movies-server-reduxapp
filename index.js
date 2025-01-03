9d63b412fb36c7326e7be6e1b311e1ad21028ce8
dahiyasunil <sunil.dahiya13@gmail.com>
2025-01-03 14:13:54 +0530

    chore: removed PORT details from log

--- index.js
+++ index.js
@@ -69,5 +69,5 @@ app.delete("/movies/:movieId", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server is up and running!", PORT);
+  console.log("Server is up and running!");
 });
